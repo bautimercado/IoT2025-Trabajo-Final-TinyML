f5f513661ea0090aaf1799577d71a6ef02592078
Bautista Mercado <bautista.mercado184770@alumnos.info.unlp.edu.ar>
2025-06-19 17:29:18 -0300

    Subo muestras viejas y sketch para recolectar muestras con microfono + sensor

---
+++ sketchs/muestras/muestras.ino
@@ -0,0 +1,48 @@
+# include <Wire.h>
+# include <Adafruit_MPU6050.h>
+# include <Adafruit_Sensor.h>
+
+Adafruit_MPU6050 mpu;
+
+# define MIC_PIN 34
+
+void setup() {
+  Serial.begin(115200);
+  while (!Serial);
+
+  if (!mpu.begin()) {
+    Serial.println("Errpr al detectar MPU6050. Verificar conexion.");
+    while (1);
+  }
+
+  mpu.setAccelerometerRange(MPU6050_RANGE_8_G);
+  mpu.setFilterBandwidth(MPU6050_BAND_5_HZ);
+
+  delay(100);
+}
+
+void loop() {
+  sensors_event_t a, g, temp;
+  bool success = mpu.getEvent(&a, &g, &temp);
+
+  if (!success) {
+    Serial.println("⚠️ Error al leer el MPU6050. Reintentando...");
+    Wire.end();
+    delay(100);
+    Wire.begin();
+    mpu.begin();
+  }
+
+  int mic_raw = analogRead(MIC_PIN);
+
+  // accX, accY, accZ, mic_signal
+  Serial.print(a.acceleration.x);
+  Serial.print(',');
+  Serial.print(a.acceleration.y);
+  Serial.print(',');
+  Serial.print(a.acceleration.z);
+  Serial.print(',');
+  Serial.println(mic_raw);
+
+  delay(100);
+}
