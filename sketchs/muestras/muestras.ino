b1348334bd492e40392f73cf8351f4b587df3a5a
Bautista Mercado <bautista.mercado184770@alumnos.info.unlp.edu.ar>
2025-06-21 18:27:35 -0300

    Subo Hz de recoleccion de muestras

--- sketchs/muestras/muestras.ino
+++ sketchs/muestras/muestras.ino
@@ -6,12 +6,15 @@ Adafruit_MPU6050 mpu;
 
 # define MIC_PIN 34
 
+unsigned long prevMillis = 0;
+const int sampleInterval = 10; // 100Hz
+
 void setup() {
   Serial.begin(115200);
   while (!Serial);
 
   if (!mpu.begin()) {
-    Serial.println("Errpr al detectar MPU6050. Verificar conexion.");
+    Serial.println("Error al detectar MPU6050. Verificar conexion.");
     while (1);
   }
 
@@ -22,27 +25,30 @@ void setup() {
 }
 
 void loop() {
-  sensors_event_t a, g, temp;
-  bool success = mpu.getEvent(&a, &g, &temp);
+  unsigned long currentMillis = millis();
+  if (currentMillis - prevMillis >= sampleInterval) {
+    prevMillis = currentMillis;
 
-  if (!success) {
-    Serial.println("⚠️ Error al leer el MPU6050. Reintentando...");
-    Wire.end();
-    delay(100);
-    Wire.begin();
-    mpu.begin();
+    sensors_event_t a, g, temp;
+    bool success = mpu.getEvent(&a, &g, &temp);
+
+    if (!success) {
+      Serial.println("⚠️ Error al leer el MPU6050. Reintentando...");
+      Wire.end();
+      delay(100);
+      Wire.begin();
+      mpu.begin();
+    }
+
+    int mic_raw = analogRead(MIC_PIN);
+
+    // accX, accY, accZ, mic_signal
+    Serial.print(a.acceleration.x);
+    Serial.print(',');
+    Serial.print(a.acceleration.y);
+    Serial.print(',');
+    Serial.print(a.acceleration.z);
+    Serial.print(',');
+    Serial.println(mic_raw);
   }
-
-  int mic_raw = analogRead(MIC_PIN);
-
-  // accX, accY, accZ, mic_signal
-  Serial.print(a.acceleration.x);
-  Serial.print(',');
-  Serial.print(a.acceleration.y);
-  Serial.print(',');
-  Serial.print(a.acceleration.z);
-  Serial.print(',');
-  Serial.println(mic_raw);
-
-  delay(100);
 }
